bb8a364831ca6a7b00911e0e5bd742419cea9e11
EvoGen3sis <192278290+EvoGen3sis@users.noreply.github.com>
2026-03-09 14:06:01 +0000

    Port defintions for cmdProc

---
+++ Assignment/working_cmd/commandProc.vhd
@@ -0,0 +1,32 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+
+entity cmdProc is 
+    port (
+        clk: in std_logic;
+        reset: in std_logic;
+
+        rxDone: out std_logic;
+        dataIn: in std_logic_vector(7 downto 0);
+        valid: in std_logic; -- dataReady in Rx port description
+        oe: in std_logic;
+        fe: in std_logic;
+
+        dataOut: out std_logic_vector(7 downto 0);
+        txNow: out std_logic;
+        txDone: in std_logic;
+
+        start: out std_logic;
+        numWords: out std_logic_vector(11 downto 0);
+        dataReady: in std_logic;
+        byte: in std_logic_vector(7 downto 0);
+        maxIndex: in std_logic_vector(11 downto 0)
+        dataResults: in std_logic_vector(55 downto 0)
+        seqDone: in std_logic;
+    );
+end cmdProc;
+
+architecture behavoural of cmdProc
+
+    begin
+    end behavoural;
