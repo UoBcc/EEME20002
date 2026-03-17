6983b72d66a8cab410712c497ea79b142b7f4453
awadmemo34 <98056829+awadmemo34@users.noreply.github.com>
2026-03-17 14:57:27 +0000

    everything changed, take a look

--- draft1.vhd
+++ draft1.vhd
@@ -2,12 +2,12 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 -- COMMON_PACK;
-
+ 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
 --library UNISIM;
 --use UNISIM.VComponents.all;
-
+ 
 entity data_processor is
     port(
     reset: in std_ulogic;
@@ -15,16 +15,15 @@ entity data_processor is
     Ctrl_2: in std_ulogic;
     data: in std_ulogic_vector(7 downto 0);
     start: in std_ulogic;
-    numWords: in std_ulogic_vector(11 downto 0); --Bianary Coded Decimal
+    numWords: in std_ulogic_vector(11 downto 0); -- Binary Coded Decimal
     ----
     Ctrl_1: out std_ulogic;
     dataReady: out std_ulogic;
     byte: out std_ulogic_vector(7 downto 0);
-    maxIndex: out std_ulogic_vector(11 downto 0); -- Bianary Coded Decimal
+    maxIndex: out std_ulogic_vector(11 downto 0); -- Binary Coded Decimal
     dataResults: out std_ulogic_vector(55 downto 0); -- switch data type here to a "character array" (whatever the fuck that is)
     seqDone: out std_ulogic); -- don't remove this bracket, its for the port function3
 end;
-
 ------------------------------------------------
 architecture arch_mealy of data_processor is
 ----------------
@@ -67,9 +66,8 @@ signal curState, nextState:state_type;
 --
 type shifting_array is array (0 to 6) of std_ulogic_vector(7 downto 0);
 signal shift_register : shifting_array;
-signal result_regsiter : shifting_array;
+signal result_register : shifting_array;
 signal post_count: integer range 0 to 3:= 0;
-signal peak_found: std_ulogic:= '0';
 --
 signal curNumWords: integer:= 0;
 signal numWords_int: integer; -- integer version of the BCD numWords
@@ -77,13 +75,12 @@ signal seqDone_sig: std_ulogic;
 signal curPeak: std_ulogic_vector(7 downto 0);
 signal curPeakIndex: integer:= 0;
 signal curPeakIndex_BCD: std_ulogic_vector(11 downto 0); -- BCD version of curPeakIndex
-signal Xn: std_ulogic_vector(7 downto 0);
 signal updateReg: std_ulogic;
 signal Ctrl_2_prev: std_ulogic;
 signal Ctrl_1_sig: std_ulogic;
 signal reg_full: std_ulogic:= '0';
--- error signals for troubleshooting
-signal error_S3_shift: std_ulogic:= '0';
+signal peak_found_proc: std_ulogic:= '0';
+signal peak_was_found: std_ulogic:= '0';
 ----------------
 begin
 ----------------
@@ -117,22 +114,25 @@ begin
                 nextState <= S0;
             else
                 nextState <= S0;
-                error_S3_shift <= '1';
             end if;
         when S4 =>
             if seqDone_sig = '1' then
                 nextState <= S0;
-            else 
+            else
                 nextState <= S0;
             end if;
         end case;
 end process;
 ----------------
-reset_counter: process(clk, reset, Ctrl_2)
+reset_counter: process(clk, reset)
 begin
-    if reset = '1' then
+    if reset = '1' then 
         curState <= S0;
-        -- set all signals to zero
+        Ctrl_2_prev <= '0';
+        curNumWords <= 0;
+        Ctrl_1_sig <= '0';
+        peak_was_found <= '0';
+        post_count <= 0;
     elsif rising_edge(clk) then
         curState <= nextState;
         Ctrl_2_prev <= Ctrl_2;
@@ -143,75 +143,87 @@ begin
             Ctrl_1_sig <= not Ctrl_1_sig;
         end if;
     end if;
-
 end process;
 ----------------
-Outputs: process(data, curState, curPeak, curPeakIndex, Xn)
+Outputs: process(clk, reset)
 begin
-    Xn <= data;
-    dataReady <= '0';
-    dataresults <= (others => '0');
-    seqDone <= '0';
-    seqDone_sig <= '0';
-    case curState is
-        when S0 => 
-            
-        when S1 =>
-            byte <= Xn;
-            dataReady <= '1';
-        when S2 =>
-            -- no outputs to be driven
-        when S3 =>
-            -- no outputs to be driven here
-        when S4 =>
-            seqDone <= '1';
-            seqDone_sig <= '1';
-            dataresults <= result_regsiter(0) & result_regsiter(1) & result_regsiter(2) & result_regsiter(3) & result_regsiter(4) & result_regsiter(5) & result_regsiter(6);
-            maxIndex <= curPeakIndex_BCD;
-    end case;
-end process;
-----------------
-DataProcessing: process(clk, curState, Data, curNumWords, curPeakIndex)
-begin
-    if rising_edge(clk) then
-        Xn <= Data;
-        if curState = S2 then
-            if Xn > curPeak then
-                curPeak <= Xn;
-                curPeakIndex <= curNumWords;
-                result_regsiter(0 to 2) <= shift_register(0 to 2);
-                result_regsiter(3) <= curPeak;
-                peak_found <= '1';
-                post_count <= 0;
-            elsif Xn < curPeak then
-                curPeak <= curPeak;
-                curPeakIndex <= curPeakIndex;
-            elsif Xn = curPeak then
-                curPeak <= Xn;
-                curPeakIndex <= curNumWords;
-                result_regsiter(0 to 2) <= shift_register(0 to 2);
-                result_regsiter(3) <= curPeak;
-                peak_found <= '1';
-                post_count <= 0;
-            end if;
-        end if;
+    if reset = '1' then
+        dataReady <= '0';
+        dataResults <= (others => '0');
+        seqDone <= '0';
+        seqDone_sig <= '0';
+        byte <= (others => '0');
+        maxIndex <= (others => '0');
+    elsif rising_edge(clk) then
+        dataReady <= '0';
+        dataResults <= (others => '0');
+        seqDone <= '0';
+        seqDone_sig <= '0';
+        byte <= (others => '0');
+        maxIndex <= (others => '0');
+        case curState is
+            when S1 =>
+                byte <= data;
+                dataReady <= '1';
+            when S4 =>
+                seqDone <= '1';
+                seqDone_sig <= '1';
+                dataResults <= result_register(0) & result_register(1) & result_register(2) & result_register(3) & result_register(4) & result_register(5) & result_register(6);
+                maxIndex <= curPeakIndex_BCD;
+                when others => null;
+        end case;
     end if;
 end process;
 ----------------
-shift_register_proc: process(clk, curState)
+DataProcessing_comp: process(curState, data, curPeak)
+begin
+    peak_found_proc <= '0';
+    if curState = S2 then
+        if data > curPeak then
+            peak_found_proc <= '1';
+        elsif data = curPeak then
+            peak_found_proc <= '1';
+        elsif data < curPeak then
+            peak_found_proc <= '0';
+        end if;
+    end if;
+end process;
+------
+DataProcessing_assign: process(clk, curState, Data, curNumWords, shift_register, curPeak, peak_found_proc)
 begin
     if rising_edge(clk) then
-        if curState = S2 or curState = S3 then 
-            shift_register(0) <= Xn;
+        if curState = S0 then
+            peak_was_found <= '0';
+            updateReg <= '0';
+        end if;
+        if curState = S2 then
+            if peak_found_proc = '1' then
+                curPeak <= data;
+                curPeakIndex <= curNumWords;
+                result_register(0 to 2) <= shift_register(0 to 2);
+                result_register(3) <= curPeak;
+            elsif peak_found_proc = '0' then
+                curPeak <= curPeak;
+                curPeakIndex <= curPeakIndex;
+            end if;
+        end if;
+    
+        if curState = S2 then
+            shift_register(0) <= data;
             shift_register(1 to 6) <= shift_register(0 to 5);
+            if peak_found_proc = '1' then 
+                peak_was_found <= '1';
+                post_count <= 0;
+            end if;
+        elsif curState = S3 then
             updateReg <= '1';
-            if peak_found = '1' then
+            if peak_was_found = '1' then
                 post_count <= post_count + 1;
-                result_regsiter(4 + post_count) <= Xn;
+                result_register(4 + post_count) <= data;                
                 if post_count = 2 then
                     reg_full <= '1';
                 end if;
-            end if;                
+            end if;
         end if;
     end if;
 end process;
